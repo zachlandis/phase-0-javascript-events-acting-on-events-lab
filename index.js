14bde5643b1af0f18caea55ad459251f1cb750a6
Zach <zachlandis91@gmail.com>
2022-12-27 14:01:37 -0700

    commit tree c1cb92723b1b48a2cf8f3e8f1f7c5d79d9d08c35

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+  document.addEventListener("keydown", function(event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+}); 
+
+const dodger = document.getElementById("dodger")
+
+function moveDodgerLeft () {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`
+  }
+
+}
+
+
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(rightNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`
+  }
+}
